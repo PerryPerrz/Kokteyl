e7a31508e1451cdbd47488d95e9b542d5dc65cc6
PerryPerrz <iophugstud6@gmail.com>
2021-12-21 10:27:51 +0100

    Résolution de bugs sur les requêtes.

--- AccesHierarchique/index.php
+++ AccesHierarchique/index.php
@@ -19,7 +19,7 @@ include_once("../StructurePage/menu.php");
             xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
         }
         var str = user + "|" + recette;
-        xmlhttp.onreadystatechange = function () {
+        xmlhttp.onreadystatechange = function() {
             if (this.readyState == 4) {
                 document.location.href = "./";
             } else {
@@ -40,7 +40,7 @@ include_once("../StructurePage/menu.php");
             xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
         }
         var str = user + "|" + recette;
-        xmlhttp.onreadystatechange = function () {
+        xmlhttp.onreadystatechange = function() {
             if (this.readyState == 4) {
                 document.location.href = "./";
             } else {
@@ -61,7 +61,7 @@ include_once("../StructurePage/menu.php");
             xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
         }
 
-        xmlhttp.onreadystatechange = function () {
+        xmlhttp.onreadystatechange = function() {
             if (this.readyState == 4) {
                 document.location.href = "./";
             } else {
@@ -83,7 +83,7 @@ include_once("../StructurePage/menu.php");
             xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
         }
 
-        xmlhttp.onreadystatechange = function () {
+        xmlhttp.onreadystatechange = function() {
             if (this.readyState == 4) {
                 document.location.href = "./";
             } else {
@@ -105,7 +105,7 @@ include_once("../StructurePage/menu.php");
             xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
         }
 
-        xmlhttp.onreadystatechange = function () {
+        xmlhttp.onreadystatechange = function() {
             if (this.readyState == 4) {
                 document.location.href = "./";
             } else {
@@ -126,7 +126,7 @@ include_once("../StructurePage/menu.php");
             xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
         }
 
-        xmlhttp.onreadystatechange = function () {
+        xmlhttp.onreadystatechange = function() {
             if (this.readyState == 4) {
                 document.location.href = "./";
             } else {
@@ -137,19 +137,18 @@ include_once("../StructurePage/menu.php");
         xmlhttp.send();
 
     }
-
 </script>
 <div id="header-wrapper">
     <?php
     //On affiche le bouton déconnexion si l'utilisateur est connecté
     if (isset($_SESSION['login'])) {
-        ?>
+    ?>
         <button onclick="location.href='../Deconnexion/'" class="button" style=vertical-align:middle>Déconnexion
         </button>
-        <?php
+    <?php
     } //On affiche le bouton connexion si l'utilisateur n'est pas connecté
     else {
-        ?>
+    ?>
         <button onclick="window.location.href = '../ConnexionSite/';" class="button" style=vertical-align:middle>
             Connexion
         </button>
@@ -196,11 +195,17 @@ include_once("../StructurePage/menu.php");
 
         function sousCategorie($categorie)
         {
-            $sql = "SELECT nom FROM SuperCategorie WHERE nomSuper = \"" . $categorie . "\"";
+            if (!str_contains($categorie, "\'")) {
+                $nomBis = str_replace("'", "\'", $categorie);
+            }
+            $sql = "SELECT nom FROM SuperCategorie WHERE nomSuper = '" . $nomBis . "'";
             $bdd = connexion();
             $req = $bdd->query($sql);
-            while ($donnes = $req->fetch()) {
-                $sql .= " OR nom IN (" . sousCategorie($donnes['nom']) . ")";
+            while ($donnees = $req->fetch()) {
+                if (!str_contains($donnees['nom'], "\'")) {
+                    $nomCat = str_replace("'", "\'", $donnees['nom']);
+                }
+                $sql .= " OR nom IN (" . sousCategorie($nomCat) . ")";
             }
             return $sql;
         }
@@ -244,165 +249,162 @@ include_once("../StructurePage/menu.php");
 
             //Si c'est le premier chargement la catégorie courante est Aliment
             if (!isset($_SESSION['categorieCourante'])) {
-            $_SESSION['categorieCourante'] = 'Aliment';
-            $_SESSION['supCategorie']['Aliment'] = 1;
-            $superCat = $bdd->query("SELECT DISTINCT nom FROM SuperCategorie WHERE nomSuper = 'Aliment'");
-            while ($donnees = $superCat->fetch()) {
-                $nomCat = $donnees['nom'];
-                $nomCat = str_replace("'", "\'", $nomCat);
-                echo "<button class='boutonCat' onclick=\"sousCat('" . $nomCat . "')\">" . $donnees['nom'] . "</button>";
-            }
+                $_SESSION['categorieCourante'] = 'Aliment';
+                $_SESSION['supCategorie']['Aliment'] = 1;
+                $superCat = $bdd->query("SELECT DISTINCT nom FROM SuperCategorie WHERE nomSuper = 'Aliment'");
+                while ($donnees = $superCat->fetch()) {
+                    $nomCat = $donnees['nom'];
+                    $nomCat = str_replace("'", "\'", $nomCat);
+                    echo "<button class='boutonCat' onclick=\"sousCat('" . $nomCat . "')\">" . $donnees['nom'] . "</button>";
+                }
             ?>
         </div><br>
-        <?php
-        }
-        //Sinon on prend la catégorie courante enregistrée
-        else{
-        if (isset($_SESSION['supCategorie'])) {
-            foreach ($_SESSION['supCategorie'] as $cat => $val) {
-                if ($cat != 'Aliment' && $val == 1) {
-                    $cat = str_replace("'", "\'", $cat);
-                    echo "<button class='boutonCat' onclick=\"backLead('$cat')\">$cat</button><br><br>";
-                }
+    <?php
             }
-        }
-        $superCat = $bdd->prepare("SELECT DISTINCT nom FROM SuperCategorie WHERE nomSuper = :super");
-        $superCat->bindParam(":super", $_SESSION['categorieCourante']);
-        $superCat->execute();
-        while ($donnees = $superCat->fetch()) {
-            $nomCat = $donnees['nom'];
-            $nomCat = str_replace("'", "\'", $nomCat);
-            echo "<button class='boutonCat' onclick=\"sousCat('" . $nomCat . "')\">" . $donnees['nom'] . "</button>";
-        }
-        ?>
+            //Sinon on prend la catégorie courante enregistrée
+            else {
+                if (isset($_SESSION['supCategorie'])) {
+                    foreach ($_SESSION['supCategorie'] as $cat => $val) {
+                        if ($cat != 'Aliment' && $val == 1) {
+                            $cat = str_replace("'", "\'", $cat);
+                            $nomAffichage = str_replace("\'", "'", $cat);
+                            echo "<button class='boutonCat' onclick=\"backLead('$cat')\">$nomAffichage</button><br><br>";
+                        }
+                    }
+                }
+                $superCat = $bdd->prepare("SELECT DISTINCT nom FROM SuperCategorie WHERE nomSuper = :super");
+                $superCat->bindParam(":super", $_SESSION['categorieCourante']);
+                $superCat->execute();
+                while ($donnees = $superCat->fetch()) {
+                    $nomCat = $donnees['nom'];
+                    $nomCat = str_replace("'", "\'", $nomCat);
+                    echo "<button class='boutonCat' onclick=\"sousCat('" . $nomCat . "')\">" . $donnees['nom'] . "</button>";
+                }
+    ?>
     </div>
     <br>
     <?php
-    }
+            }
 
-    // On détermine sur quelle page on se trouve
-    if (isset($_GET['page']) && !empty($_GET['page'])) {
-        $currentPage = (int)strip_tags($_GET['page']);
-    } else {
-        $currentPage = 1;
-    }
+            // On détermine sur quelle page on se trouve
+            if (isset($_GET['page']) && !empty($_GET['page'])) {
+                $currentPage = (int)strip_tags($_GET['page']);
+            } else {
+                $currentPage = 1;
+            }
 
-    //On donne le chemin des images nécessaire à l'affichage des images des cocktails
-    $cheminImage = "../Photos/";
+            //On donne le chemin des images nécessaire à l'affichage des images des cocktails
+            $cheminImage = "../Photos/";
+            $nomCat = str_replace("'", "\'", $_SESSION['categorieCourante']);
+            $queryNbCocktails = $bdd->query("SELECT COUNT(DISTINCT nom) AS nbCocktails FROM Recettes JOIN Liaison ON Liaison.nomRecette = Recettes.nom WHERE nomIngredient = '" . $nomCat . "' OR  nomIngredient IN (" . sousCategorie($_SESSION['categorieCourante']) . ")");
+            if ($resultQuery = $queryNbCocktails->fetch()) {
+                //On récupère le nombre de cocktails et fait nos calculs pour les afficher correctement avec la pagination
 
-    $queryNbCocktails = $bdd->query("SELECT COUNT(DISTINCT nom) AS nbCocktails FROM Recettes JOIN Liaison ON Liaison.nomRecette = Recettes.nom WHERE nomIngredient = '" . $_SESSION['categorieCourante'] . "' OR  nomIngredient IN (" . sousCategorie($_SESSION['categorieCourante']) . ")");
-    if ($resultQuery = $queryNbCocktails->fetch()) {
-    //On récupère le nombre de cocktails et fait nos calculs pour les afficher correctement avec la pagination
+                //On récupère le nombre de cocktails
+                $nbCocktails = (int)$resultQuery['nbCocktails'];
 
-    //On récupère le nombre de cocktails
-    $nbCocktails = (int)$resultQuery['nbCocktails'];
+                // On détermine le nombre de cocktails par page
+                $parPage = 8;
 
-    // On détermine le nombre de cocktails par page
-    $parPage = 8;
+                // On calcule le nombre de pages total
+                $nbPages = ceil($nbCocktails / $parPage);
 
-    // On calcule le nombre de pages total
-    $nbPages = ceil($nbCocktails / $parPage);
+                // Calcul du 1er cocktail de la page
+                $premier = ($currentPage * $parPage) - $parPage;
 
-    // Calcul du 1er cocktail de la page
-    $premier = ($currentPage * $parPage) - $parPage;
+                //On récupère les cocktails à afficher pour cette page
+                $queryCocktails = $bdd->query("SELECT DISTINCT nom FROM Recettes JOIN Liaison ON Liaison.nomRecette = Recettes.nom WHERE nomIngredient = '" . $nomCat . "' OR  nomIngredient IN (" . sousCategorie($_SESSION['categorieCourante']) . ")");
+                if ($resultQuery2 = $queryCocktails->fetchAll()) {
+                    //On récupère tous les cocktails à afficher sous un format utilisable en sql
+                    $listeStrCocktails = "";
+                    for ($i = 0; $i < sizeof($resultQuery2); $i++) {
+                        $temp = str_replace("'", "\'", $resultQuery2[$i]['nom']);
+                        $listeStrCocktails .= "'" . $temp . "',";
+                    }
+                    $listeStrCocktails = rtrim($listeStrCocktails, ","); //On enlève la dernière virgule
+                    $queryCocktails2 = $bdd->prepare("SELECT * FROM Recettes WHERE nom IN (" . $listeStrCocktails . ") ORDER BY nom LIMIT :premier, :parpage;");
+                    $queryCocktails2->bindValue(':premier', $premier, PDO::PARAM_INT);
+                    $queryCocktails2->bindValue(':parpage', $parPage, PDO::PARAM_INT);
 
-    //On récupère les cocktails à afficher pour cette page
-    $queryCocktails = $bdd->query("SELECT DISTINCT nom FROM Recettes JOIN Liaison ON Liaison.nomRecette = Recettes.nom WHERE nomIngredient = '" . $_SESSION['categorieCourante'] . "' OR  nomIngredient IN (" . sousCategorie($_SESSION['categorieCourante']) . ")");
-
-    if ($resultQuery2 = $queryCocktails->fetchAll()) {
-    //On récupère tous les cocktails à afficher sous un format utilisable en sql
-    $listeStrCocktails = "";
-    for ($i = 0; $i < sizeof($resultQuery2); $i++) {
-        $temp = str_replace(")", " ", str_replace("(", " ", str_replace("'", "", $resultQuery2[$i]['nom'])));
-        $listeStrCocktails .= "'" . $temp . "',";
-    }
-    $listeStrCocktails = rtrim($listeStrCocktails, ","); //On enlève la dernière virgule
-
-    $queryCocktails2 = $bdd->prepare("SELECT * FROM Recettes WHERE nom IN (" . $listeStrCocktails . ") ORDER BY nom LIMIT :premier, :parpage;");
-
-    $queryCocktails2->bindValue(':premier', $premier, PDO::PARAM_INT);
-    $queryCocktails2->bindValue(':parpage', $parPage, PDO::PARAM_INT);
-
-    // On exécute la requète
-    $queryCocktails2->execute();
-    $cocktails = $queryCocktails2->fetchAll(PDO::FETCH_ASSOC);
+                    // On exécute la requète
+                    $queryCocktails2->execute();
+                    $cocktails = $queryCocktails2->fetchAll(PDO::FETCH_ASSOC);
     ?>
-    <!-- !PAGE CONTENT! -->
-    <div class="w3-main w3-content w3-padding" style="max-width:1200px;margin-top:100px">
+        <!-- !PAGE CONTENT! -->
+        <div class="w3-main w3-content w3-padding" style="max-width:1200px;margin-top:100px">
 
-        <!-- First Photo Grid-->
-        <div class="w3-row-padding w3-padding-16 w3-center" id="drink">
+            <!-- First Photo Grid-->
+            <div class="w3-row-padding w3-padding-16 w3-center" id="drink">
 
-            <?php
-            for ($i = 0; $i < $parPage / 2; ++$i) {
-                //On ne parcourt pas les élements vides de l'Array
-                if (!empty($cocktails[$i])) {
-                    $nomImage = formatageString($cocktails[$i]['nom']);
-                    $nomFinal = $cheminImage . $nomImage . ".jpg";
-                    ?>
-                    <div class="w3-quarter">
-                        <div class="img">
-                            <img src="<?= $nomFinal ?>" alt="<?= $cocktails[$i]['nom'] ?>" class="images">
+                <?php
+                    for ($i = 0; $i < $parPage / 2; ++$i) {
+                        //On ne parcourt pas les élements vides de l'Array
+                        if (!empty($cocktails[$i])) {
+                            $nomImage = formatageString($cocktails[$i]['nom']);
+                            $nomFinal = $cheminImage . $nomImage . ".jpg";
+                ?>
+                        <div class="w3-quarter">
+                            <div class="img">
+                                <img src="<?= $nomFinal ?>" alt="<?= $cocktails[$i]['nom'] ?>" class="images">
+                            </div>
+                            <h3> <?= $cocktails[$i]['nom'] ?></h3>
+                            <p>Ingrédients
+                                : <?= str_replace("|", ", ", $cocktails[$i]['ingredients']) //On remplace les | dans la description par des , pour rendre la description plus lisible  
+                                    ?></p>
+                            <p>Préparation : <?= $cocktails[$i]['preparation'] ?></p>
                         </div>
-                        <h3> <?= $cocktails[$i]['nom'] ?></h3>
-                        <p>Ingrédients
-                            : <?= str_replace("|", ", ", $cocktails[$i]['ingredients']) //On remplace les | dans la description par des , pour rendre la description plus lisible  ?></p>
-                        <p>Préparation : <?= $cocktails[$i]['preparation'] ?></p>
-                    </div>
                 <?php }
-            }
-            ?>
-        </div>
+                    }
+                ?>
+            </div>
 
-        <!-- Second Photo Grid-->
-        <div class="w3-row-padding w3-padding-16 w3-center">
-            <?php
+            <!-- Second Photo Grid-->
+            <div class="w3-row-padding w3-padding-16 w3-center">
+                <?php
 
-            for ($i = $parPage / 2; $i < $parPage; ++$i) {
-                //On ne parcourt pas les élements vides de l'Array
-                if (!empty($cocktails[$i])) {
-                    $nomImage = formatageString($cocktails[$i]['nom']);
-                    $nomFinal = $cheminImage . $nomImage . ".jpg";
-                    ?>
-                    <div class="w3-quarter">
-                        <div class="img">
-                            <img src="<?= $nomFinal ?>" alt="<?= $cocktails[$i]['nom'] ?>" class="images">
+                    for ($i = $parPage / 2; $i < $parPage; ++$i) {
+                        //On ne parcourt pas les élements vides de l'Array
+                        if (!empty($cocktails[$i])) {
+                            $nomImage = formatageString($cocktails[$i]['nom']);
+                            $nomFinal = $cheminImage . $nomImage . ".jpg";
+                ?>
+                        <div class="w3-quarter">
+                            <div class="img">
+                                <img src="<?= $nomFinal ?>" alt="<?= $cocktails[$i]['nom'] ?>" class="images">
+                            </div>
+                            <h3> <?= $cocktails[$i]['nom'] ?></h3>
+                            <p>Ingrédients
+                                : <?= str_replace("|", ", ", $cocktails[$i]['ingredients']) //On remplace les | dans la description par des , pour rendre la description plus lisible  
+                                    ?></p>
+                            <p>Préparation : <?= $cocktails[$i]['preparation'] ?></p>
                         </div>
-                        <h3> <?= $cocktails[$i]['nom'] ?></h3>
-                        <p>Ingrédients
-                            : <?= str_replace("|", ", ", $cocktails[$i]['ingredients']) //On remplace les | dans la description par des , pour rendre la description plus lisible  ?></p>
-                        <p>Préparation : <?= $cocktails[$i]['preparation'] ?></p>
-                    </div>
+
                 <?php }
-            }
-            ?>
+                    }
+                ?>
+            </div>
             <!-- Pagination -->
             <div class="w3-center w3-padding-32">
                 <div class="w3-bar">
                     <!-- Lorsque l'on clique sur la flèche précedante, cela ramène l'utilisateur sur la page précedente, si celle-ci n'existe pas, le bouton n'est pas cliquable-->
-                    <a href="./?page=<?= $currentPage - 1 ?>"
-                       class="w3-bar-item w3-button w3-hover-black <?= ($currentPage == 1) ? "disable" : "" ?>">«</a>
+                    <a href="./?page=<?= $currentPage - 1 ?>" class="w3-bar-item w3-button w3-hover-black <?= ($currentPage == 1) ? "disable" : "" ?>">«</a>
 
                     <!-- On créer des boutons qui permettent de naviguer entre les pages, si on est sur la page correspondante à un bouton, celui-ci est mis en valeur-->
                     <?php for ($i = 1; $i <= $nbPages; ++$i) {
-                        ?>
-                        <a href="./?page=<?= $i ?>"
-                           class="<?= ($currentPage == $i) ? "w3-bar-item w3-black w3-button" : "w3-bar-item w3-button w3-hover-black" ?>"><?= $i ?></a>
+                    ?>
+                        <a href="./?page=<?= $i ?>" class="<?= ($currentPage == $i) ? "w3-bar-item w3-black w3-button" : "w3-bar-item w3-button w3-hover-black" ?>"><?= $i ?></a>
                     <?php } ?>
 
                     <!-- Lorsque l'on clique sur la flèche suivante, cela ramène l'utilisateur sur la page suivante, si celle-ci n'existe pas, le bouton n'est pas cliquable-->
-                    <a href=" ./?page=<?= $currentPage + 1 ?>"
-                       class="w3-bar-item w3-button w3-hover-black <?= ($currentPage == $nbPages) ? "disable" : "" ?>">»</a>
+                    <a href=" ./?page=<?= $currentPage + 1 ?>" class="w3-bar-item w3-button w3-hover-black <?= ($currentPage == $nbPages) ? "disable" : "" ?>">»</a>
                 </div>
             </div>
             <hr id="about">
+
+    <?php
+                }
+            } ?>
         </div>
-
-        <?php
-        }
-        } ?>
-
-    </div>
 </div>
 
 <?php
